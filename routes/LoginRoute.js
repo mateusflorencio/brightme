f44081273556470475e3539fe4a90215bcb30f92
MaryNishizaki <mnishizaki@hotmail.com>
2022-05-28 17:41:26 -0300

    rotas, controles e ejs de produto, login, cadastro usuario e painel usuario

---
+++ routes/LoginRoute.js
@@ -0,0 +1,8 @@
+const express = require ("express")
+const LoginController = require ("../controllers/LoginController
+
+const router = express.Router()
+
+router.get ("/", LoginController.index)
+
+module.exports = router
