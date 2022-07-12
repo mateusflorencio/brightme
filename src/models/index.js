26bc08c9894329b4fae6070a1b443c713220b02e
mateus <mateusflorencio@outlook.com.br>
2022-07-12 11:41:39 -0300

    refactor: movendo varios exports apenas para um documento

--- src/models/index.js
+++ src/models/index.js
@@ -3,11 +3,18 @@ const Cliente = require('./cliente/cliente')
 const Telefone = require('./cliente/telefone')
 const Endereco = require('./cliente/endereco')
 
+// path pedido
+const Cupom = require('./pedido/cupom')
+const OrdemDePedido = require('./pedido/ordem-de-pedido')
+const Pedido = require('./pedido/pedido')
+const Status = require('./pedido/status')
+
 //path produto
 const Categoria = require('./produto/categoria')
 const Estoque = require('./produto/estoque')
 const Fabricante = require('./produto/fabricante')
 const Produto = require('./produto/produto')
+const Promocao = require('./produto/promocao')
 
 //path resources
 const Image = require('./resources/image')
@@ -17,6 +24,7 @@ const Administrador = require('./user/adm')
 
 module.exports = {
   Cliente, Telefone, Endereco, Categoria, Estoque,
-  Fabricante, Produto, Image, Administrador
+  Fabricante, Produto, Image, Administrador, Cupom, 
+  OrdemDePedido, Pedido, Status
 }
 
