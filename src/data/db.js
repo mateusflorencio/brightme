b0c1e26f3c81ea51da7f214a78bf6e2b6b6becb9
mateus <mateusflorencio@outlook.com.br>
2022-07-13 13:42:13 -0300

    feat: algoritimo para criar todas as tabelas

--- src/data/db.js
+++ src/data/db.js
@@ -17,6 +17,7 @@ start: async function start(){
 try {
   await sequelize.authenticate()
   console.log('Connection has been established successfully.')
+  await sequelize.sync({ force: true})
 } catch (error) {
   console.error('Unable to connect to the database:', error)
 }},
