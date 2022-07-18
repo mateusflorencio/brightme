e38f10e3439a7e90aa56405854eac356b7646a12
mateus <mateusflorencio@outlook.com.br>
2022-07-18 18:47:52 -0300

    fix: arrumando linha de comando

--- script/generateModel.sh
+++ script/generateModel.sh
@@ -16,14 +16,14 @@ npx sequelize-cli model:generate --name Cupom --attributes ativo:BOOLEAN,codigo:
 
 npx sequelize-cli model:generate --name Promocao --attributes ativo:BOOLEAN,produtoId:INTEGER,porcentagen:INTEGER
 
-npx sequelize-cli model:genrate --name Kit --attributes ativo:BOOLEAN,produtoId:ARRAY:INTEGER,titulo:STRING,descricao:STRING,imageId:ARRAY:INTEGER
+npx sequelize-cli model:generate --name Kit --attributes ativo:BOOLEAN,produtoId:ARRAY:INTEGER,titulo:STRING,descricao:STRING,imageId:ARRAY:INTEGER
 
 npx sequelize-cli model:generate --name Endereco --attributes cep:INTEGER,numero:INTEGER,logradouro:STRING,bairro:STRING,complemento:STRING,UF:STRING,municipio:STRING,referencia:STRING
 
 npx sequelize-cli model:generate --name StatusOS --attributes status:STRING
 
-npx sequelize-cli model:generate --name Pedido --attributes clienteId:INTEGER,produtoId:INTEGER,subtotal:DOUBLE,cupomId:INTEGER,total:DOUBLE,frete:DOUBLE
+npx sequelize-cli model:generate --name Pedido --attributes produtoId:INTEGER,subtotal:DOUBLE,cupomId:INTEGER,total:DOUBLE,frete:DOUBLE
 
-npx sequelize-cli model:generate --name OrdemDeServico --atttibutes pedidoId:INTEGER,enderecoId:INTEGER,statusOPId:INTEGER,clienteId:INTEGER
+npx sequelize-cli model:generate --name OrdemDeServico --attributes pedidoId:INTEGER,enderecoId:INTEGER,statusOPId:INTEGER,clienteId:INTEGER
 
-npx sequelize-cli model:generate --name Cliente --attribustes nome:STRING,sobrenome:STRING,senha:STRING,email:STRING,telefone:INTEGER,imageId:INTEGER,cpf:STRING,enderecoId:INTEGER,ordemDeServicoId:INTEGER
+npx sequelize-cli model:generate --name Cliente --attributes nome:STRING,sobrenome:STRING,senha:STRING,email:STRING,telefone:INTEGER,imageId:INTEGER,cpf:STRING,enderecoId:ARRAY:INTEGER,ordemDeServicoId:INTEGER
