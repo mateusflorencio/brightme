3881378fc0bbaba3b60f8d0457c08805e69d40a1
mateus <mateusflorencio@outlook.com.br>
2022-07-18 23:33:35 -0300

    feat: add migration telefone

---
+++ database/migrations/20220719023143-create-telefone.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Telefones', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      numero: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Telefones');
+  }
+};
