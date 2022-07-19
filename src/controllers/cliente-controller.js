9907caf89ac1998ddde225930b3793cba4d20727
mateus <mateusflorencio@outlook.com.br>
2022-07-19 15:46:07 -0300

    fix: remove alguns cógigos desnecessários em controller e services

--- src/controllers/cliente-controller.js
+++ src/controllers/cliente-controller.js
@@ -2,14 +2,8 @@ const ClienteRepository = require('../services/cliente-repository')
 
 const clienteController = {
     create: async (req, res) => {
-       await ClienteRepository.create("a", "b", "b", "d", "e", "f", "g")
-        res.send('ok')
     },
     login: (req, res) =>{
-        const { email, password } = req.body
-        if(!email && !password)
-        res.render('login')
-       res.send(email)
     }
 }
 
--- src/services/cliente-repository.js
+++ src/services/cliente-repository.js
@@ -1,15 +1,8 @@
-const { User } = require('../models')
 
 const ClienteRepository = {
-  login: async function (email, password) {
-    const response = await User.findOne({ where: { email: email} })
-    return response
+  login: async function () {
   },
-  create: async function (nome, sobrenome, password, email, cpf, enderecoId, telefoneId) {
-    //const passwordEncrypted = await encrypt(email, password)
-    const cliente = {nome:'mateus',sobrenome:'florencio',senha:'senhaEncript',
-      email:'email2@mail.com'}
-    const response = await User.create(cliente)
+  create: async function () {
   }
 }
 
