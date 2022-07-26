d1c237a989a141b89fb8daddfc8a927e56ca5ed1
mateus <mateusflorencio@outlook.com.br>
2022-07-25 22:14:26 -0300

    fix: arrumando tamanho das entradas na validacao do body

--- src/controllers/validacoes/validacao.js
+++ src/controllers/validacoes/validacao.js
@@ -4,9 +4,12 @@ const validacaoCadastro = () => {
   return  [
     body('nome').notEmpty(),
     body('sobrenome').notEmpty(),
-    body('telefone').isLength({min:10, max:11}).notEmpty(),
+    body('telefone').isLength({min:9, max:14}),
     body('email').isEmail().notEmpty(),
-    body('senha').isLength({min:6}).isAlphanumeric().notEmpty()]
+    body('CPF').isLength({min:10, max:14}),
+    body('senha').isLength({min:6}).isAlphanumeric().notEmpty(),
+    body('confirmacaoSenha').notEmpty(),
+    ]
 }
 
 
