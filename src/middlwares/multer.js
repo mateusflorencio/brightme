2e20feaf726b80a1b63a14ffd715a29367d79633
mateus <mateusflorencio@outlook.com.br>
2022-08-03 10:47:03 -0300

    feat: multer salva image com id do produto

--- src/middlwares/multer.js
+++ src/middlwares/multer.js
@@ -1,5 +1,22 @@
-const multer = require('multer');
-const upload = multer({ dest: 'public/image/produtos' });
+const multer = require('multer')
+const { ProdutoRepository } = require('../repository')
+
+const prodRepo = new ProdutoRepository()
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/image/produtos')
+  },
+  filename: async (req, file, cb) => {
+    const result = await prodRepo.buscarTodos()
+    const size = result.length
+    const extensaoArquivo = file.originalname.split('.')[1]
+    cb(null, `prod${size + 1}-${req.files.length}.${extensaoArquivo}`)
+  }
+})
+
+const upload = multer({ storage })
 
 module.exports = {
   upload
