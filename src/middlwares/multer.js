25c39b323560ae51aafc9b7230f7e8f502ddab07
mateus <mateusflorencio@outlook.com.br>
2022-08-04 13:42:05 -0300

    fix: foi criado um log para o multer saber mesmo com o bd apagado saber o id da imagem seguinte

--- src/middlwares/multer.js
+++ src/middlwares/multer.js
@@ -1,8 +1,9 @@
 const multer = require('multer')
+const fs = require('fs')
 const { ProdutoRepository } = require('../repository')
 
 const prodRepo = new ProdutoRepository()
-
+const pathLog = 'src/middlwares/multer-log.txt'
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -10,13 +11,22 @@ const storage = multer.diskStorage({
   },
   filename: async (req, file, cb) => {
     const result = await prodRepo.buscarTodos()
-    const size = result.length
     const extensaoArquivo = file.originalname.split('.')[1]
-    cb(null, `prod${size + 1}-${req.files.length}.${extensaoArquivo}`)
+
+    const response = result.length > 0 ? result[result.length - 1].id : readRegisterLog()
+    const id = parseInt(response)
+
+    fs.writeFileSync(pathLog, `${id + 1}`)
+    cb(null, `prod${id + 1}-${req.files.length}.${extensaoArquivo}`)
   }
 })
 
 const upload = multer({ storage })
+
+const readRegisterLog = () => fs.readFileSync(pathLog, 'utf8', (err, data) => {
+  if (err) { console.log(err) }
+  return data.trim()
+})
 
 module.exports = {
   upload
