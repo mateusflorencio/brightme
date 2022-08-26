f8a810eaca5b442ae241bcc1be4c71ed9b14f370
mateus <mateusflorencio@outlook.com.br>
2022-08-26 11:38:30 -0300

    fix: alterando password para senha

--- src/services/add-cliente-to-db.js
+++ src/services/add-cliente-to-db.js
@@ -7,9 +7,9 @@ const fs = require('fs')
 const cliRepo = new ClienteRepository()
 const imgPadrao = fs.readFileSync('public/image/padrao/image-padrao.jpg', 'base64')
 
-const addClienteToDB = async ({ nome, sobrenome, password, email, telefone, CPF }) => {
+const addClienteToDB = async ({ nome, sobrenome, senha, email, telefone, CPF }) => {
   
-  const hashedPassword = await encrypt(password)
+  const hashedPassword = await encrypt(senha)
   const result = await cliRepo.cadastro(new NewClienteDTO(nome, sobrenome, hashedPassword, email, telefone, CPF))
 
   fs.writeFileSync(`public/image/clientes/cliente${result.id}`, `data:image/jpeg;base64,${imgPadrao}`)
